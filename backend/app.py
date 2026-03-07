00aaff2d25598214bb6c80aa60242a40774ad162
AlmuHusan <38337359+AlmuHusan@users.noreply.github.com>
2026-03-07 18:09:02 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -62,7 +62,7 @@ def get_books():
         }
         x = requests.post(url, json=myobj, headers={"Authorization": "Bearer " + apiKey})
         resX = json.loads(x.text)
-        print(resX["data_array"])
+        print(resX['result']["data_array"])
         return jsonify(resX) , 200
     except Exception as e:
         print("Failed")
