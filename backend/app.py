29b2f0533567bd0ca7c7d3d86a4e246d08169174
AlmuHusan <38337359+AlmuHusan@users.noreply.github.com>
2026-04-16 12:29:56 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -131,7 +131,7 @@ async def vectorSearch():
             query={
                 "inputs": {"text": request.json["queryString"]},
                 "top_k": 5,
-                "filter":{"source":request.json["filterDocuments"]}
+                "filter":{"source":[f for f in request.json["filterDocuments"]]}
             }
         )
         # print(data["result"]["hits"])
