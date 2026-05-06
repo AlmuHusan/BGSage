2417c3ab207538fcb74c1b5bf507f04a425c5ff4
AlmuHusan <38337359+AlmuHusan@users.noreply.github.com>
2026-05-06 16:42:47 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -69,7 +69,7 @@ def askBGSage():
             model="llama-3.3-70b-versatile",
         )
         print(chat_completion)
-        statement = "INSERT into messages (chat_role,content,sid) VALUES (\"system\",\"{}\",{})".format(chat_completion.choices[0].message.content, sid)
+        statement = "INSERT into messages (chat_role,content,sid) VALUES (\"assistant\",\"{}\",{})".format(chat_completion.choices[0].message.content, sid)
         print(statement)
         dbAPiBody["statement"] = statement
         x = requests.post(url, json=dbAPiBody, headers={"Authorization": "Bearer " + apiKey})
